72b01b5415bfc4ce6e68c47494acefa0956b216f
kendrang <kendrang@users.noreply.github.com>
2019-11-10 20:45:21 -0500

    added a test

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -3,10 +3,21 @@ package models;
 public class Sneaker {
 
     private int id;
+
+
+
     private String name;
     private String brand;
     private String sport;
     private int size;
     private int qty;
     private float price;
+
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/services/SneakerServiceTest.java
+++ src/test/java/services/SneakerServiceTest.java
@@ -1,4 +1,22 @@
 package services;
 
+import models.Sneaker;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SneakerServiceTest {
-}
+
+        @Test
+        public void setNameTest() {
+            // given (1)
+            String expected = "OZWEEGO";
+
+            // when (2)
+            Sneaker testSneaker = new Sneaker();
+            testSneaker.setName(expected);
+
+            // then (3)
+            Assertions.assertEquals(expected, testSneaker.getName());
+        }
+    }
+
