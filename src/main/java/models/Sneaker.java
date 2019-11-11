e1341142f992ab57b5f9f5e53df033c1b8e3aee3
kendrang <kendrang@users.noreply.github.com>
2019-11-10 20:53:47 -0500

    new branch soon

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -3,9 +3,6 @@ package models;
 public class Sneaker {
 
     private int id;
-
-
-
     private String name;
     private String brand;
     private String sport;
@@ -14,10 +11,59 @@ public class Sneaker {
     private float price;
 
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
+
     public String getName() {
         return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
     }
 }
