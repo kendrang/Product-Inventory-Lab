85f15d2e7645fed3590684b9bc2031079edaeb1d
kendrang <kendrang@users.noreply.github.com>
2019-11-10 21:37:17 -0500

    part3

---
+++ src/main/java/io/console.java
@@ -0,0 +1,15 @@
+package io;
+public class console {
+
+    public static class Console {
+        public static void printWelcome(){
+            System.out.println("" +
+                    "**************************************************" +
+                    "***           Welcome and Bienvenue            ***" +
+                    "***                    to                      ***" +
+                    "***          ZipCo Inventory Manager           ***" +
+                    "**************************************************");
+        }
+
+    }
+}
